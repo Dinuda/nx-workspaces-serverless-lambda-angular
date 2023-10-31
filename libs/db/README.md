78a943640dab09abfbe051f7f9bca217a99d70d6
Dinuda <ydinuda@gmail.com>
2023-10-31 18:38:18 +0530

    ff

---
+++ libs/db/README.md
@@ -0,0 +1,13 @@
+# db
+
+This library was generated with [Nx](https://nx.dev).
+
+
+
+
+
+## Running unit tests
+
+Run `nx test db` to execute the unit tests via [Jest](https://jestjs.io).
+
+
---
+++ libs/db/src/lib/client.ts
@@ -0,0 +1,30 @@
+import * as AWS from 'aws-sdk';
+import { DynamoDB } from 'aws-sdk';
+
+
+import { env } from '@nx-workspaces-serverless-lambda-angular/env';
+
+AWS.config.update({
+  region: env.region,
+});
+
+let client: DynamoDB | null = null;
+
+export function getClient() {
+  if (!client) {
+    const options = {
+      endpoint: env.dynamo.endpoint,
+      httpOptions: {
+        connectTimeout: 1000,
+        timeout: 1000,
+      },
+    };
+
+    client = new DynamoDB(env.name === 'dev' ? options : undefined);
+  }
+
+  return {
+    db: client,
+    TableName: env.dynamo.tableName,
+  };
+}
